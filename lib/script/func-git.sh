cd29709b3fac030dc0970c1e3e3ba05333679bac
Vitalik Mironov <v.mironov@citydrive.ru>
2022-01-02 06:33:07 +0300

    make some functions

--- lib/script/func-git.sh
+++ lib/script/func-git.sh
@@ -31,3 +31,28 @@ function gg_cmt() {
 
     return 0
 }
+
+function gg_msg() {
+    local log_msg=$(git log -n 1 --format=%B)
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    print $log_msg
+
+    return 0
+}
+
+function gg_cmt_with_last_msg() {
+    local log_msg="$(gg_msg)"
+    if [ $? -ne 0 ]; then
+        gg_err "error while comitting"
+        return 1
+    fi
+
+    gg_cmt $1 "$log_msg"
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
