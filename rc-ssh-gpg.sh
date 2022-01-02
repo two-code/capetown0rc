b5889ef2717dc5acff7572be53e7c1873b93a752
Vitalik Mironov <v.mironov@citydrive.ru>
2022-01-02 06:42:13 +0300

    make some functions

--- rc-ssh-gpg.sh
+++
@@ -1,7 +0,0 @@
-unset SSH_AGENT_PID
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-export GPG_TTY=$(tty)
-
-gpg-connect-agent updatestartuptty /bye >/dev/null
-
-gpgconf --launch gpg-agent
