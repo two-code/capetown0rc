878361531b812afc18fada20686228172d260c37
Vitalik Mironov <v.mironov@citydrive.ru>
2022-05-08 15:15:45 +0300

    add grub

--- lib/script/aliases.sh
+++ lib/script/aliases.sh
@@ -19,3 +19,5 @@ alias k8='kubectl'
 alias __go-vet='go mod tidy && go mod vendor && go vet ./...'
 
 alias __ps='ps -eLfMlyZ kuid,pid --cumulative'
+
+alias ww_lsblk='sudo lsblk --sort PARTLABEL -o NAME,SIZE,PHY-SEC,LOG-SEC,RA,WSAME,FSTYPE,TYPE,UUID,PARTUUID,PARTLABEL,MOUNTPOINT'
