97979893c7933c4259e7f6a90d011100b7a67f07
Vitalik Mironov <v.mironov@citydrive.ru>
2022-05-08 22:41:15 +0300

    change alias(es) prefix

--- lib/script/aliases.sh
+++ lib/script/aliases.sh
@@ -1,23 +1,22 @@
-alias __ls='ls -alghF'
-alias __ls-r='ls -alghFR1'
-alias __lsf='ls -alghF'
-alias __rm='rm -fRvd'
+alias ww_ls='ls -alghF'
+alias ww_lsr='ls -alghFR1'
+alias ww_lsf='ls -alghF'
+alias ww_rm='rm -fRvd'
 
-alias __dc-up-rebuild='docker-compose up --build -d --remove-orphans -V'
-alias __dc-start='docker-compose start'
-alias __dc='docker-compose'
-alias __d-prune='docker system prune'
+alias ww_dc-up-rebuild='docker-compose up --build -d --remove-orphans -V'
+alias ww_dc-start='docker-compose start'
+alias ww_dc='docker-compose'
+alias ww_d-prune='docker system prune'
 
-alias __sht='shutdown -P now'
-alias __rbt='shutdown --reboot now'
+alias ww_sht='shutdown -P now'
+alias ww_rbt='shutdown --reboot now'
 
-alias mi='microk8s'
-alias mikk='microk8s kubectl'
+alias ww_mi='microk8s'
+alias ww_mik='microk8s kubectl'
 alias kk='kubectl'
-alias k8='kubectl'
 
-alias __go-vet='go mod tidy && go mod vendor && go vet ./...'
+alias ww_go-vet='go mod tidy && go mod vendor && go vet ./...'
 
-alias __ps='ps -eLfMlyZ kuid,pid --cumulative'
+alias ww_ps='ps -eLfMlyZ kuid,pid --cumulative'
 
 alias ww_lsblk='sudo lsblk --sort PARTLABEL -o NAME,SIZE,FSAVAIL,PHY-SEC,LOG-SEC,FSTYPE,TYPE,UUID,PARTUUID,PARTLABEL,MOUNTPOINT'
