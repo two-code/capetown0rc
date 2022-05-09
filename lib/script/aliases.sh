d035b0be86d1d9ebb6c98b8a2db9a18d12210943
Vitalik Mironov <v.mironov@citydrive.ru>
2022-05-09 19:54:34 +0300

    change grub fonts

--- lib/script/aliases.sh
+++ lib/script/aliases.sh
@@ -19,4 +19,4 @@ alias ww_go-vet='go mod tidy && go mod vendor && go vet ./...'
 
 alias ww_ps='ps -eLfMlyZ kuid,pid --cumulative'
 
-alias ww_lsblk='sudo lsblk --sort PARTLABEL -o NAME,SIZE,FSAVAIL,PHY-SEC,LOG-SEC,FSTYPE,TYPE,UUID,PARTUUID,PARTLABEL,MOUNTPOINT'
+alias ww_lsblk='sudo lsblk --sort PARTLABEL -o NAME,PARTLABEL,LABEL,SIZE,FSAVAIL,PHY-SEC,LOG-SEC,FSTYPE,TYPE,UUID,PARTUUID,MOUNTPOINT'
